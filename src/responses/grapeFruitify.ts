75649b25171f34704c535ad51b487a62dcf568be
dernoson <dernoson@gmail.com>
2023-03-04 22:29:38 +0800

    add wording

--- src/responses/grapeFruitify.ts
+++ src/responses/grapeFruitify.ts
@@ -3,15 +3,17 @@ import { MessageResponser } from './types';
 export const grapeFruitify: MessageResponser = {
     desc: 'YT實況主的神軸提供(不一定回應，關鍵字：軸)',
     handler(message) {
-        const randNum = Math.floor(Math.random() * 6);
+        const randNum = Math.floor(Math.random() * 10);
         if (!message.content.includes('軸')) return;
         return responseStr[randNum];
     },
 };
 
 const responseStr = [
-    '你知道遠牙開技能會降敵人防禦嗎？',
+    '你知道遠牙開技能會無視物防嗎？',
     '我覺得因為早露比不上號角，所以不要練早露。',
     'W的閃避機率高達40%！！！',
     '黑的出場率很爛，像我淵默都不放黑的。',
+    '崖心2技其實是大力喔，長知識了吧！',
+    '伏擊客的那種收藏品，攻擊力增加+100% 可是周圍不能放幹員',
 ];
