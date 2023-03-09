cecd106505451eaffb8bb7e6d441739047b30348
dernoson <dernoson@gmail.com>
2023-03-09 17:51:06 +0800

    fix: force silence priority

--- src/responses/index.ts
+++ src/responses/index.ts
@@ -8,10 +8,10 @@ import { saileach } from './saileach';
 import { forceSilence, getSilenceState } from './forceSilence';
 
 export const extraResponse = (message: Message<true>) => {
-    if (message.content == '機器人閉嘴') return forceSilence();
-    if (getSilenceState()) return;
     const pluginKey = botEnv.get('ResponsePlugin');
     if (typeof pluginKey != 'string') return;
+    if (message.content == '機器人閉嘴') return forceSilence();
+    if (getSilenceState()) return;
     const handler = responsePlugins[pluginKey]?.handler;
     if (!handler) return;
     return handler(message);
