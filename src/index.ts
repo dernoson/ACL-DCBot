f81e52e38345ef84ae13678169b84187fe0f17c8
dernoson <dernoson@gmail.com>
2024-08-20 15:24:51 +0800

    feat: BotToken is save in files/secret.json

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,76 @@
 import { BaseGuildTextChannel, Client, REST, Routes } from 'discord.js';
-import { BotToken, BotClientID } from './secret/tokens';
 import { getMatchStorage, recoverMatchStorage } from './match';
 import { extraResponse } from './responses';
 import { Help, commandDefs, commandFunctions } from './commands';
 import { clientOptions } from './consts';
 import { hasSendMessagePermission } from './functions';
 import { getEnv, initEnv, readConfig } from './config';
+import { readJson } from './fileHandlers';
 
-const helpCommandBuilder = Help.getBuilder();
-const requestDataBody = commandDefs.concat(helpCommandBuilder);
-const interactionExecutes = { ...commandFunctions, [helpCommandBuilder.name]: Help.getExecute() };
+const main = async () => {
+    const helpCommandBuilder = Help.getBuilder();
+    const requestDataBody = commandDefs.concat(helpCommandBuilder);
+    const interactionExecutes = {
+        ...commandFunctions,
+        [helpCommandBuilder.name]: Help.getExecute(),
+    };
 
-new REST()
-    .setToken(BotToken)
-    .put(Routes.applicationCommands(BotClientID), { body: requestDataBody })
-    .then(() => console.log('Successfully reloaded application (/) commands.'))
-    .catch((error) => console.error(error));
+    const { BotToken, BotClientID } = await readJson('secret.json', () => initBotInfo);
 
-const client = new Client(clientOptions);
+    new REST()
+        .setToken(BotToken)
+        .put(Routes.applicationCommands(BotClientID), { body: requestDataBody })
+        .then(() => console.log('Successfully reloaded application (/) commands.'))
+        .catch((error) => console.error(error));
 
-client.on('ready', async (client) => {
-    await readConfig();
-    initEnv(client);
-    await recoverMatchStorage();
+    const client = new Client(clientOptions);
 
-    isBotReady = true;
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+    client.on('ready', async (client) => {
+        await readConfig();
+        initEnv(client);
+        await recoverMatchStorage();
 
-client.on('messageCreate', async (message) => {
-    try {
-        if (!isBotReady) return;
-        if (!message.inGuild() || message.author.bot) return;
-        if (!hasSendMessagePermission(message.guild, message.channel)) return;
+        isBotReady = true;
+        console.log(`Logged in as ${client.user.tag}!`);
+    });
 
-        if (message.channelId == getEnv().logChannel?.id) return;
-        if (message.channel instanceof BaseGuildTextChannel && getMatchStorage(message.channel)) return;
+    client.on('messageCreate', async (message) => {
+        try {
+            if (!isBotReady) return;
+            if (!message.inGuild() || message.author.bot) return;
+            if (!hasSendMessagePermission(message.guild, message.channel)) return;
 
-        const resp = extraResponse(message);
-        if (resp) await message.reply(resp);
-    } catch (error) {
-        console.log(error);
-    }
-});
+            if (message.channelId == getEnv().logChannel?.id) return;
+            if (message.channel instanceof BaseGuildTextChannel && getMatchStorage(message.channel)) return;
 
-client.on('interactionCreate', async (interaction) => {
-    try {
-        if (!interaction.isChatInputCommand()) return;
-        if (!interaction.inGuild() || !interaction.guild || !interaction.channel) return;
-        if (!hasSendMessagePermission(interaction.guild, interaction.channel)) return;
-        if (!isBotReady) await interaction.reply('機器人尚未就緒');
+            const resp = extraResponse(message);
+            if (resp) await message.reply(resp);
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-        await interactionExecutes[interaction.commandName]?.(interaction);
-    } catch (error) {
-        console.log(error);
-    }
-});
+    client.on('interactionCreate', async (interaction) => {
+        try {
+            if (!interaction.isChatInputCommand()) return;
+            if (!interaction.inGuild() || !interaction.guild || !interaction.channel) return;
+            if (!hasSendMessagePermission(interaction.guild, interaction.channel)) return;
+            if (!isBotReady) await interaction.reply('機器人尚未就緒');
 
-client.login(BotToken);
+            await interactionExecutes[interaction.commandName]?.(interaction);
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-let isBotReady = false;
+    client.login(BotToken);
+
+    let isBotReady = false;
+};
+
+main();
+
+const initBotInfo = {
+    BotToken: '',
+    BotClientID: '',
+};
